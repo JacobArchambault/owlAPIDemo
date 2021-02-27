9ca1efe06b28bd8fc52f21936d94a04de7bc41fc
Jacob Archambault <jacobarchambault@gmail.com>
2021-02-27 17:17:33 -0500

    Add JOWLClass() to JOWLData and PrefixedData

--- src/main/java/com/jacobarchambault/owldemo/JOWLData.java
+++ src/main/java/com/jacobarchambault/owldemo/JOWLData.java
@@ -1,12 +1,38 @@
 package com.jacobarchambault.owldemo;
 
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.PrefixManager;
 
-public class JOWLData implements JData {
-	JOntology ontology;
-	OWLDataFactory dataFactory;
-	JOWLData(JOntology ontology){
+public final class JOWLData implements JData {
+	final JOntology ontology;
+	final OWLDataFactory dataFactory;
+
+	JOWLData(
+			JOntology ontology) {
 		this.ontology = ontology;
 		dataFactory = ontology.ontologyData();
+	}
+
+	final OWLClass JOWLClass(
+			IRI fromIri) {
+		return dataFactory.getOWLClass(
+				fromIri);
+	}
+
+	final OWLClass JOWLClass(
+			String fromString) {
+		return JOWLClass(
+				IRI.create(
+						fromString));
+	}
+
+	final OWLClass JOWLClass(
+			String fromString,
+			PrefixManager withPrefix) {
+		return dataFactory.getOWLClass(
+				fromString,
+				withPrefix);
 	}
 }
--- src/main/java/com/jacobarchambault/owldemo/PrefixedData.java
+++ src/main/java/com/jacobarchambault/owldemo/PrefixedData.java
@@ -1,5 +1,24 @@
 package com.jacobarchambault.owldemo;
 
-public class PrefixedData implements JData {
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.PrefixManager;
+
+public final class PrefixedData implements JData {
+	final JOWLData jOWLData;
+	final PrefixManager prefix;
+
+	PrefixedData(
+			JOWLData jOWLData,
+			PrefixManager prefix) {
+		this.jOWLData = jOWLData;
+		this.prefix = prefix;
+	}
+
+	final OWLClass JOWLClass(
+			String fromString) {
+		return jOWLData.JOWLClass(
+				fromString,
+				prefix);
+	}
 
 }
