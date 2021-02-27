d31e5eb3bff6a85f1ffb3462d08fe87f778d4888
Jacob Archambault <jacobarchambault@gmail.com>
2021-02-27 13:17:34 -0500

    move load and save actions into JOntology and remove JOntologyManager

--- src/main/java/com/jacobarchambault/owldemo/App.java
+++ src/main/java/com/jacobarchambault/owldemo/App.java
@@ -3,10 +3,8 @@ package com.jacobarchambault.owldemo;
 import java.io.File;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.semanticweb.owlapi.model.OWLOntologyStorageException;
 
 /**
@@ -17,18 +15,15 @@ public class App {
 	public static void main(
 			String[] args) throws OWLOntologyCreationException,
 			OWLOntologyStorageException {
-		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-		OWLOntology ontology = manager.loadOntologyFromOntologyDocument(
+		JOntology jOntology = new JOntology(
 				new File(
-						"solar-ontology.owl"));
+						"solar-ontology.owl"),
+				OWLManager.createConcurrentOWLOntologyManager());
+		OWLOntology ontology = jOntology.load();
 
-		System.out.println(
-				"Hello");
-		IRI iri = IRI.create(
-				new File(
-						"solar-ontology-result.owl").toURI());
-		manager.saveOntology(
+		jOntology.save(
 				ontology,
-				iri);
+				new File(
+						"solar-ontology-result.owl"));
 	}
 }
--- src/main/java/com/jacobarchambault/owldemo/JOntology.java
+++ src/main/java/com/jacobarchambault/owldemo/JOntology.java
@@ -2,24 +2,43 @@ package com.jacobarchambault.owldemo;
 
 import java.io.File;
 
+import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.OWLOntologyStorageException;
 
 public final class JOntology {
 
 	final File file;
-	final JOntologyManager manager;
+	final OWLOntologyManager manager;
 
 	JOntology(
 			File file,
-			JOntologyManager manager) {
+			OWLOntologyManager manager) {
 		this.file = file;
 		this.manager = manager;
 	}
 
 	final OWLOntology load() throws OWLOntologyCreationException {
-		return manager.create()
-				.loadOntologyFromOntologyDocument(
-						file);
+		return manager.loadOntologyFromOntologyDocument(
+				file);
+	}
+
+	final void save(
+			OWLOntology ontology,
+			File file) throws OWLOntologyStorageException {
+		manager.saveOntology(
+				ontology,
+				IRI.create(
+						file.toURI()));
+	}
+
+	final void save(
+			OWLOntology ontology) throws OWLOntologyStorageException {
+		manager.saveOntology(
+				ontology,
+				IRI.create(
+						file.toURI()));
 	}
 }
